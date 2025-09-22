2a868aa5a5e064b3ce8a7f40b334de6e2fbb880e
muhamad faris <m.faris5670@gmail.com>
2025-09-22 17:12:31 +0700

    fix edu

--- src/components/EducationSection.tsx
+++ src/components/EducationSection.tsx
@@ -1,13 +1,13 @@
 function EducationSection(){
     return(
-    <section>
-        <div>
-            <h2> Education</h2>
-            <div>
-                <div>
-                    <h3>Mahasiswa Software Engginering</h3>
-                    <p>Telkom University Purwokerto</p>
-                    <p>Web Development</p>
+    <section id="education" className="py20">
+        <div className="max-w-4xl mx-auto px-6">
+            <h2 className="text-3xl font-bold mb-8 text-center"> Education</h2>
+            <div className="space-y-6">
+                <div className="bg-white shadow-md p-6 rounded-lg">
+                    <h3 className="text-xl font-semibold">Mahasiswa Software Engginering</h3>
+                    <p className="text-gray-600">Telkom University Purwokerto</p>
+                    <p className="mt-2">Web Development</p>
                 </div>
             </div>
         </div>
