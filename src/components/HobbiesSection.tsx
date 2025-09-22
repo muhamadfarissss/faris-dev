ea6d61b080f0d6a70a6371926e84a424667ee5ce
muhamad faris <m.faris5670@gmail.com>
2025-09-22 17:04:23 +0700

    hobbies

---
+++ src/components/HobbiesSection.tsx
@@ -0,0 +1,14 @@
+function HobbiesSection(){
+    return(
+        <section>
+            <div>
+                <h2>Hobbies</h2>
+                <p>
+                    Outside of Coding, I enjoy Editing Videos and Playing Chess.
+                    I also love exploring new technologies and staying updated with the latest trends in the tech world.
+                </p>
+            </div>
+        </section>
+    )
+}
+export default HobbiesSection
