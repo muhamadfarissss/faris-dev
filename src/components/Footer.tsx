f0bf2ab2e1b83174a39609cc8f804a0a6fa43c6e
muhamad faris <m.faris5670@gmail.com>
2025-09-22 17:04:29 +0700

    footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,8 @@
+function Footer(){
+    return(
+        <footer>
+            <p>@ {new Date().getFullYear()} Muhamad Faris. All right reserved</p>
+        </footer>
+    )
+}
+export default Footer
