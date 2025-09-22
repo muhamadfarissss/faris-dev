77a232eb94bf250d74bfcf4f560d5cebb6a51568
muhamad faris <m.faris5670@gmail.com>
2025-09-22 16:49:21 +0700

    education

---
+++ src/components/EducationSection.tsx
@@ -0,0 +1,14 @@
+function EducationSection(){
+    <section>
+        <div>
+            <h2> Education</h2>
+            <div>
+                <div>
+                    <h3>Mahasiswa Software Engginering</h3>
+                    <p>Telkom University Purwokerto</p>
+                    <p>Web Development</p>
+                </div>
+            </div>
+        </div>
+    </section>
+}
