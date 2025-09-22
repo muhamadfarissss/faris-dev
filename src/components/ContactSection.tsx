bf0529db000b0202223f17084c8185de11ef8425
muhamad faris <m.faris5670@gmail.com>
2025-09-22 17:04:16 +0700

    contact

---
+++ src/components/ContactSection.tsx
@@ -0,0 +1,14 @@
+function ContactSection(){
+    return(
+        <section>
+            <div>
+                <h2>Contact Me</h2>
+                <p>
+                    Feel free to reach out for collaborations or just a friendly hello!
+                </p>
+                <a href="mailto=m.faris5670@gmail.com">Send Email </a>
+            </div>
+        </section>
+    )
+}
+export default ContactSection
