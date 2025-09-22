dfe90854f0be66fcdacb82b272af85ee010f215a
muhamad faris <m.faris5670@gmail.com>
2025-09-22 17:21:55 +0700

    fixskills

--- src/components/SkillsSection.tsx
+++ src/components/SkillsSection.tsx
@@ -1,12 +1,12 @@
 function SkillsSection(){
-    const skills = ["javascript", "PHP", "HTML", "CSS", "Node.js", "MySQL", "Phyton"]
+    const skills = ["Javascript", "PHP", "HTML", "CSS", "Node.js", "MySQL", "Phyton"]
     return(
-        <section>
-            <div>
-                <h2>Skills</h2>
-                <div>
+        <section id="skills" className="py-20">
+            <div className="max-w-4xl mx-auto px-6 text-center" >
+                <h2 className="text-3xl font-bold mb-8">Skills</h2>
+                <div className="flex flex-wrap justify-center gap-4">
                     {skills.map(skill => (
-                        <span>
+                        <span key ={skill} className="px-4 py-2 bg-blue-100 text-blue-600 rounded-full shadow">
                             {skill}
                         </span>
                     ))}
