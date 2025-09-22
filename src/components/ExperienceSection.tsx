8df00032494738b01efde01b5e8af842fbe44dab
muhamad faris <m.faris5670@gmail.com>
2025-09-22 16:49:30 +0700

    experiences

---
+++ src/components/ExperienceSection.tsx
@@ -0,0 +1,18 @@
+function ExperienceSection(){
+    return (
+        <section>
+            <div>
+                <h2>Experience</h2>
+                <div>
+                    <div>
+                        <h3>Marketing Divisi Editing</h3>
+                        <p>Prima Cyber Team | 2022</p>
+                        <p>Editing Video Promotion with After Effect and CorelDraw</p>
+                    </div>
+
+                </div>
+            </div>
+        </section>
+    )
+}
+export default ExperienceSection
